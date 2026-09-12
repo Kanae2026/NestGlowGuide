cea1e15c8e7d916f6937cc5c157e0281707c7bdf
Kanae2026 <info@kanae-inc.com>
2026-09-12 10:34:50 +0900

    Update en.js

--- js/lang/en.js
+++ js/lang/en.js
@@ -42,7 +42,7 @@ window.translations.en = {
 
     ci_link_text: "Guest Registration Form",
 
-    ci_step2_title: "Access (Getting to Your Room)",
+    ci_step2_title: "Directions from the station to the keybox and room entry",
 
     ci_step2_desc: "For directions to your room, please check the \"Route Guide from the Station\" on the Access page.",
 
